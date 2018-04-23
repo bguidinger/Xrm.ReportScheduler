672ec24706857e9c5bcd4d0fa12d29f5582c878b
Bob Guidinger <bguidinger@andosi.com>
2018-04-23 06:47:30 -0500

    Updated Word/Excel rendering options to docx/xlsx.

--- src/Plugins/Reports/Render.cs
+++ src/Plugins/Reports/Render.cs
@@ -17,13 +17,17 @@
 
             // Get input parameters
             var reportId = provider.Target.Id;
-            var format = provider.ExecutionContext.InputParameterOrDefault<string>("Format");
+            var format = provider.ExecutionContext.InputParameterOrDefault<string>("Format").ToUpper();
+            if (format == "WORD" || format == "EXCEL")
+            {
+                format = format + "OPENXML";
+            }
             var parameters = provider.ExecutionContext.InputParameterOrDefault<string>("Parameters");
 
             // Render the report
             var renderer = new Renderer(resource, username, password);
             var report = renderer.Render(reportId, format, parameters);
-            
+
             // Return as Base-64
             provider.ExecutionContext.OutputParameters["Output"] = Convert.ToBase64String(report);
         }
